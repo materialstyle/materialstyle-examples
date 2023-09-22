354202bbe632834b1f23a5d32ebc9e4e9c582a38
Neeraj Kumar Das <nkdas91@gmail.com>
2023-09-22 11:55:48 +0530

    Fix ripple in sass-js example

--- 3.1/sass-js/js/main.js
+++ 3.1/sass-js/js/main.js
@@ -4,6 +4,11 @@
 
 import "../node_modules/@materialstyle/materialstyle/dist/js/materialstyle.bundle.min.js";
 
+const rippleSurface = [].slice.call(document.querySelectorAll('.ripple-surface'))
+rippleSurface.map(function (ripple) {
+  return new mdc.ripple.MDCRipple(ripple)
+})
+
 // Initialize Text Fields
 var textFieldList = [].slice.call(document.querySelectorAll('.form-control'))
 textFieldList.map(function (textField) {
